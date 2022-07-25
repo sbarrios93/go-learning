ce3d8b4bab46a7fc337aa0d52f1cc482b9db261e
sbarrios93 <sbarrios93@gmail.com>
2022-07-24 19:08:16 -0700

    first chapter

--- learn-with-tests/hello.go
+++ learn-with-tests/hello.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
@@ -14,8 +16,32 @@ func Hello(name string, language string) string {
 	if language == spanish {
 		return spanishHelloPrefix + name
 	}
+	if language == french {
+		return frenchHelloPrefix + name
+	}
 	return englishHelloPrefix + name
 }
+
+func HelloSwitch(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return grettingPrefix(language) + name
+}
+
+func grettingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
 func main() {
 	fmt.Println(Hello("world", ""))
 }
--- learn-with-tests/hello_test.go
+++ learn-with-tests/hello_test.go
@@ -25,5 +25,30 @@ func TestHello(t *testing.T) {
 		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Frances", "French")
+		want := "Bonjour, Frances"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("[SWITCH] saying hello to people", func(t *testing.T) {
+		got := HelloSwitch("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("[SWITCH] say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := HelloSwitch("", "")
+		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("[SWITCH] in Spanish", func(t *testing.T) {
+		got := HelloSwitch("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("[SWITCH] in French", func(t *testing.T) {
+		got := HelloSwitch("Frances", "French")
+		want := "Bonjour, Frances"
+		assertCorrectMessage(t, got, want)
+	})
 }
