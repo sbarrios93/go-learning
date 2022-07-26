1e28b65d04ffbcde2c66d9eec75da275dc7c0605
sbarrios93 <sbarrios93@gmail.com>
2022-07-26 08:13:12 -0700

    init

--- learn-with-tests/arrays-slices/sum.go
+++ learn-with-tests/arrays-slices/sum.go
@@ -1,9 +1,15 @@
 package main
 
+import "fmt"
+
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
 }
